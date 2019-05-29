c575bedab3c0390e53057e7f02b4d68cd573e8f6
pooya parsa <pooya@pi0.ir>
2019-05-30 02:17:02 +0430

    update tests

--- test/defaults.test.js
+++ test/defaults.test.js
@@ -1,22 +1,21 @@
 const { setupMockNuxt } = require('./_utils')
 
-describe('browserBaseURL', () => {
+describe('defaults', () => {
   let nuxt
 
   beforeAll(async () => {
     nuxt = await setupMockNuxt({
-      http: {
-        browserBaseURL: '/test_api',
-        retry: true
-      }
+      http: {}
     })
   })
 
-  test('browserBaseURL', () => {
+  test('should render template with defaults', () => {
     expect(nuxt.moduleContainer.addTemplate).toBeDefined()
     const call = nuxt.moduleContainer.addTemplate.mock.calls.find(args => args[0].src.includes('plugin.js'))
     const options = call[0].options
     expect(options.baseURL).toBe('http://localhost:3000/')
-    expect(options.browserBaseURL).toBe('/test_api')
+    expect(options.browserBaseURL).toBe('http://localhost:3000/')
+    expect(options.clientTimeout).toBe(false)
+    expect(options.serverTimeout).toBe(false)
   })
 })
