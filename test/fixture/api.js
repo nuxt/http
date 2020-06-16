7056bf1da38a77c1f084ded447b006bc4a9ba28e
pooya parsa <pooya@pi0.ir>
2020-06-16 20:40:16 +0200

    test: add error fixture

--- test/fixture/api.js
+++ test/fixture/api.js
@@ -1,6 +1,11 @@
 module.exports = {
   path: '/test_api',
   handler (req, res) {
+    if (req.url === '/error') {
+      res.statusCode = 418
+      res.end('I\'m a teapot')
+    }
+
     res.end(JSON.stringify({
       url: req.url,
       method: req.method
---
+++ test/fixture/pages/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Response: {{ res }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ app }) {
+    const res = await app.$http.$get('error')
+    return { res }
+  }
+}
+</script>
