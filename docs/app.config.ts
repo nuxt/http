c7d7f449ca12a677f00d9e573367a618b23db547
Sébastien Chopin <seb@nuxtjs.com>
2022-12-07 13:36:07 +0100

    update

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -2,11 +2,11 @@ export default defineAppConfig({
   docus: {
     url: 'https://http.nuxtjs.org',
     header: {
-      title: false,
       logo: true
     },
     socials: {
       twitter: 'nuxt_js',
+      github: 'nuxt/http'
     },
   }
 })
