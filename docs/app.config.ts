204c26129d52e429f5fa55178fcaf1e23b132412
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-07 01:00:47 +0100

    feat(docs): upgrade to latest Docus

---
+++ docs/app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+  docus: {
+    url: 'https://http.nuxtjs.org',
+    header: {
+      title: false,
+      logo: true
+    },
+    socials: {
+      twitter: 'nuxt_js',
+    },
+  }
+})
